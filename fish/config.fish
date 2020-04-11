5a55253c11cd3eb79b41829ebeedc44447238807
Lokesh Mohanty <lokesh1197@gmail.com>
2020-04-11 10:39:01 +0530

    fish: add ssh-agent if not added yet

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,12 @@ fish_vi_key_bindings
 set PATH ~/.local/bin $PATH
 set -x EDITOR nvim
 set -x VISUAL nvim
+
+
+if test -S ~/.ssh/ssh_auth_sock 
+else
+  eval (ssh-agent -c)
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+end
+set -x SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add
