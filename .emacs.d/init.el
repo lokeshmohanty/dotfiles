3b464ae34e48e47601308272c61dfe93ec4cff12
Lokesh Mohanty <lokesh1197@gmail.com>
2021-10-24 14:59:06 +0530

    emacs: move emacs config to another repo and use as a submodule

--- .emacs.d/init.el
+++
@@ -1,8 +0,0 @@
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
-
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-      (load custom-file))
