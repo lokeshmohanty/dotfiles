411cdc2300ad9845bab3e78e73424c9beb44fc31
Lokesh Mohanty <lokesh1197@gmail.com>
2022-05-23 11:51:22 +0530

    fish: update PATH environment variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,25 @@
 fish_vi_key_bindings
 
+set PATH ~/Applications/flutter/bin $PATH
+set PATH ~/.bin $PATH
+
+# Add GOPATH/bin to path
+set PATH ~/go/bin $PATH
+
+# Add android/sdk/tools/bin to path
+set PATH ~/Android/Sdk/tools/bin $PATH
+
+# Add .local/bin and .bin to path
 set PATH ~/.local/bin $PATH
+
+# Add cabal and ghcup binaries to path
+set PATH ~/.cabal/bin $PATH
+set PATH ~/.ghcup/bin $PATH
+
 set -x EDITOR emacsclient
 set -x VISUAL emacsclient
 
-nvm use v14.16.0
+# nvm use v14.18.2
 
 source ~/.shell_aliases
 # if test -e ~/.shell_aliases
